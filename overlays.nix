8006a14d2c5ec9995e5821ca98bf06286c5216c7
SewoShun <93058265+SewoShun@users.noreply.github.com>
2025-12-31 16:18:35 +0900

    feat: add default overlay

--- overlays.nix
+++ overlays.nix
@@ -1,33 +1,40 @@
 {
   inputs,
   lib,
+  withSystem,
   ...
 }:
-let
-  extraVimPluginsSrc = {
-    inherit (inputs)
-      blink-cmp-skkeleton-src
-      atone-nvim-src
-      jj-nvim-src
-      lazyjj-nvim-src
-      skkeleton-src
-      ;
-  };
-  extraVimPluginsPackages =
-    pkgs:
-    lib.attrsets.mapAttrs' (
-      name: src:
-      (lib.attrsets.nameValuePair (lib.strings.removeSuffix "-src" "${name}") (
-        pkgs.vimUtils.buildVimPlugin { inherit name src; }
-      ))
-    ) extraVimPluginsSrc;
-
-  extraVimPluginsOverlay = final: prev: {
-    vimPlugins = prev.vimPlugins // extraVimPluginsPackages prev;
-  };
-in
 {
   flake.overlays = {
-    extraVimPlugins = extraVimPluginsOverlay;
+    default =
+      final: prev:
+      withSystem prev.stdenv.hostPlatform.system (
+        { config, ... }:
+        {
+          neovim = config.packages.default;
+        }
+      );
+    extraVimPlugins =
+      final: prev:
+      let
+        extraVimPluginsSrc = {
+          inherit (inputs)
+            blink-cmp-skkeleton-src
+            atone-nvim-src
+            jj-nvim-src
+            lazyjj-nvim-src
+            skkeleton-src
+            ;
+        };
+        extraVimPluginsPackages = lib.attrsets.mapAttrs' (
+          name: src:
+          (lib.attrsets.nameValuePair (lib.strings.removeSuffix "-src" "${name}") (
+            prev.vimUtils.buildVimPlugin { inherit name src; }
+          ))
+        ) extraVimPluginsSrc;
+      in
+      {
+        vimPlugins = prev.vimPlugins // extraVimPluginsPackages;
+      };
   };
 }
